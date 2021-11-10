572147a53ebb8f8ad8b1f5c931f5f6460911180c
Krishna Teja <78008005+Kcode17@users.noreply.github.com>
2021-11-10 17:27:46 -0500

    Side Bar operation with useState"

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React,{ useState} from 'react'
 import styled from "styled-components"
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+
     return (
         <Container>
             <Logo src = "/images/logo.svg" />
@@ -17,8 +20,25 @@ function Header() {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <HamburgerMenu />
+                <CustomMenu onClick={()=>setBurgerStatus(true)}/>
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={()=>setBurgerStatus(false)} />
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Test Drive</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+                <li><a href="#">Semi</a></li>
+                <li><a href="#">Charging</a></li>
+                <li><a href="#">Powerwall</a></li>
+                <li><a href="#">Utilities</a></li>
+                <li><a href="#">Find Us</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Investor Relations</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -35,6 +55,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index:1;
 
 `
 const Logo = styled.img`
@@ -73,7 +94,44 @@ const RightMenu = styled.div`
    
 
 `
-const HamburgerMenu = styled(MenuIcon)`
+const CustomMenu = styled(MenuIcon)`
     cursor: pointer;
 
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom:0;
+    right:0;
+    background: white;
+    width:300px;
+    z-index:2;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
+    transition: transform 0.2s ease-in-out;
+    li{
+        padding: 15px 0;
+    
+
+        a{
+            font-weight:450;
+        }
+    }    
+`
+
+const CustomClose = styled(CloseIcon)
+`
+    cursor: pointer;
+    
+`
+
+const CloseWrapper = styled.div
+`
+display: flex;
+justify-content: flex-end;
 `
